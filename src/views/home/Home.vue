4db76aa1b54c892c6ba8e3f424d5b7aedbcf3060
jiangzikai <3061846560@qq.com>
2021-07-25 11:28:45 +0800

    首页请求数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -7,13 +7,32 @@
 <script>
 
   import NavBar from "components/common/navbar/NavBar";
+  import {getHomeMultidata} from "network/home";
 
-    export default {
+  export default {
       name: "Home",
       components: {
         NavBar
+      },
+      data() {
+        return {
+          // result: null
+          banners: [],
+          recommends: []
+        }
+      },
+      // 组件创建完毕
+      created() {
+          // 1.请求多个数据
+          // this 箭头函数向上找this,就是这里的this,这个this就是vue对象
+          getHomeMultidata().then(res => {
+              // console.log(res);
+              // this.result = res;
+            this.banners = res.data.banner.list;
+            this.recommends = res.data.recommend.list;
+          })
       }
-    }
+  }
 </script>
 
 <style scoped>
